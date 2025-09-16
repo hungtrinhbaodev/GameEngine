689ddcdb7152422caae239ed54aca9df2a56fd36
hung9n2 <hungtb@vng.com.vn>
2025-09-16 23:21:39 +0700

    [glm_utils]: add scale, rotation, translation matrix from param.

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
 #include <chrono>
 #include <glm/glm.hpp>
-#include <glm/matrix.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 #include <thread>
 #include <utility/glm_utils.h>
 
 int main() {
-    glm::mat3 m(2);
-    std::cout << m << std:: endl;
-    glm::vec3 v(2);
-    std::cout << v << std:: endl;
-    std::cout << m * v << std::endl;
+    glm::mat4 scale = Utility::Glm::make_scale(1.5f, 1.2f, 0.5f);
+    glm::mat4 rotation = Utility::Glm::make_rotation(60.f, 60.f, 45.f);
+    std::cout << scale << std::endl;
+    std::cout << rotation << std::endl;
     return 0;
 }
